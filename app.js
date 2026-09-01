f7940f958e784ca402c6939b6e39f73c7cd44205
Ph32 <130022373+ri32ph@users.noreply.github.com>
2026-09-01 15:49:11 +0900

    ver.1.1

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ document.getElementById('saveContact').addEventListener('click', () => {
     'FN:池田 理恵',
     'TITLE:薬剤師・博士（臨床薬学）',
     'ORG:こはく堂薬局 / キミのミライ・ラボ',
+    'ADR;TYPE=WORK:;;東小路町10-16;諫早市;長崎県;;日本',
     'TEL;TYPE=CELL:080-3999-9153',
     'EMAIL;TYPE=INTERNET:rikeda32@gmail.com',
     'URL:https://kohakudo589.com',
     'URL:https://melab365.vercel.app',
+    'URL:https://ph-info.info',
     'END:VCARD'
   ].join('\r\n');
   const blob = new Blob([vcard], { type: 'text/vcard;charset=utf-8' });
